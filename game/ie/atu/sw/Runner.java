470a32a5311ca79c47b5fad04fa431b9a66312ac
mcciaa24 <cian.mc_cann@student.reutlingen-university.de>
2025-03-28 20:39:55 +0000

    Moved image to base directory

---
+++ game/ie/atu/sw/Runner.java
@@ -0,0 +1,20 @@
+package ie.atu.sw;
+
+import static java.lang.System.*;
+import javax.swing.SwingUtilities;
+
+public class Runner {
+	public static void main(String[] args) throws Exception {
+		
+		/*
+		 * Always run a GUI in a separate thread from the main thread.
+		 */
+		SwingUtilities.invokeAndWait(() -> { //Sounds like the Command Pattern at work!
+			try {
+				new GameWindow();
+			} catch (Exception e) {
+				out.println("[ERROR] Yikes...problem starting up " + e.getMessage());
+			}
+		});
+	}
+}
